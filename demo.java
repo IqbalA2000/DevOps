f272d0483a53e7488290f22a3e8f5d3d7a8ac126
iqbal <219110176@psu.edu.sa>
2022-09-12 08:41:55 +0300

    my first commit

---
+++ demo.java
@@ -0,0 +1,11 @@
+Public class demo {
+
+public static void main(String[] args){
+System.out.println("welcome to java");
+System.out.println("welcome to DevOps");
+System.out.println("Git is fun");
+
+
+}
+
+}
