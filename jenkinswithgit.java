ce7754a20522086cf0876157061a67435cafae65
iqbal <219110176@psu.edu.sa>
2022-10-01 15:25:12 +0300

    pushing jenkinswithgit.java0

---
+++ jenkinswithgit.java
@@ -0,0 +1,15 @@
+Public class jenkinswithgit{
+
+public static void main(String[] args){
+
+int charperline= 0;
+for(int i=33;i<256;charperline++){
+System.out.println((char)i=((charperline%10==0)?"\n":"\t"));
+
+System.out.println("");
+
+}
+
+}
+
+}
